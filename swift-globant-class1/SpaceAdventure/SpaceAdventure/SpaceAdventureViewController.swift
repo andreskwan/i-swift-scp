77372b14dce2fc7b45df469c07872a47ef6dd191
kwana008 <Andres.X.Kwan.-ND@Disney.com>
2015-10-27 11:27:51 -0500

    Added - support for keyboard - textfield delegate methods

--- swift-globant-class1/SpaceAdventure/SpaceAdventure/SpaceAdventureViewController.swift
+++ swift-globant-class1/SpaceAdventure/SpaceAdventure/SpaceAdventureViewController.swift
@@ -8,28 +8,95 @@
 
 import UIKit
 
-class SpaceAdventureViewController: UIViewController {
+class SpaceAdventureViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var answer: UITextField!
+    
+    var keyBoardHeight: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        answer.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        answer.textAlignment = NSTextAlignment.Center
+        answer.delegate = self
         
     }
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBarHidden = true
+        subscribeToKeyboardNotifications()
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.navigationBarHidden = false
+                unsubscribeFromKeyBoardNotifications()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    // MARK: UITextFieldDelegate - Methods
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+    }
+    func textFieldDidEndEditing(textField: UITextField) {
+//        (textField.text?.isEmpty != true)
+        if let text = textField.text {
+            switch text {
+                case "YES":
+                    print("yes")
+                case "NO":
+                    print("no")
+                default:
+                    print("Sorry, I didn't get that. Please answer YES or NO.   ")
+            }
+        }
+    }
     
+    // MARK: Keyboard
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    func unsubscribeFromKeyBoardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:UIKeyboardWillHideNotification, object: nil)
+    }
+    func keyboardWillShow(notification: NSNotification) {
+        if answer.isFirstResponder() {
+            keyBoardHeight = getKeyboardHeight(notification)
+            if keyBoardHeight != 0 {
+                animateTextField(true)
+            }
+        }
+    }
+    func keyboardWillHide(notification: NSNotification) {
+        if answer.isFirstResponder() && keyBoardHeight != nil {
+            animateTextField(false)
+        }
+    }
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        if let userInfo = notification.userInfo {
+            if let keyboardSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue() {
+                return keyboardSize.height
+            }
+        }
+        return 0
+    }
+    func animateTextField(up: Bool) {
+        let movement = (up ? -keyBoardHeight : keyBoardHeight)
+        UIView.animateWithDuration(0.3, animations: {
+            self.view.frame = CGRectOffset(self.view.frame, 0, movement)
+        })
+    }
 }
 
